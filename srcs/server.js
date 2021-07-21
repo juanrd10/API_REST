804c17913c5924d318f3998c3e27acddae8b8b18
Juan Rodriguez secades <juanrodr@c3r6s3.42madrid.com>
2021-07-21 17:46:39 +0200

    add rest function to call the expertises and get out from postman

--- srcs/server.js
+++ srcs/server.js
@@ -7,7 +7,6 @@ let myheaders
 let opt_get
 let token
 let acs_token
-let next
 
 app.get('/', async(req, res) =>{
 	res.redirect('https://api.intra.42.fr/oauth/authorize?client_id=' + process.env. CLIENT_ID + '&redirect_uri=http%3A%2F%2F' + process.env.IP + '%3A3000%2Findex&response_type=code')
@@ -30,39 +29,25 @@ app.get('/static/personal/:id', async(req, res) =>{
 	})
 })
 
-app.get('/static/personal/:id', async(req, res) =>{
-	console.log(req.params.id)
-	const response = await fetch("https://api.intra.42.fr/v2/users/" + req.params.id , opt_get, (error, meta, body)=>{
-		const data = JSON.parse(body.toString())
-		console.log("/v2/me -> " + token);
-		res.render(__dirname + '/public/static/personal.ejs', {me: data, req_ret:''})
-	})
-})
-
 app.get('/static/expertises/:id', async(req, res) =>{
 	const response = await fetch("https://api.intra.42.fr/v2/users/" + req.params.id + "/expertises_users", opt_get, (error, meta, body)=>{
 		console.log(req.params.id)
 		console.log(body)
 		const data = JSON.parse(body.toString())
-		console.log("hhskfhakjkf")
 		console.log(data);
-		next = meta.responseHeaders.link
-		if (next)
-		{
-			let next_index = next.lastIndexOf("https:")
-			let last = next.lastIndexOf(">")
-			next = next.substring(next_index, last);
-			console.log(next)
-			console.log(next_index)
-		}
 		console.log("/v2/expertises/" + req.params.id + " -> " + token);
 		res.render(__dirname + '/public/static/expertises.ejs', {expertises: data, req_ret:''})
 	})
 })
 
+app.get('/rest/expertises/:id', async(req, res) =>{
+	const response = await fetch("https://api.intra.42.fr/v2/users/" + req.params.id + "/expertises_users", opt_get, (error, meta, body)=>{
+		const data = JSON.parse(body.toString())
+		res.json(data)
+	})
+})
 
 app.get('/index?*', async(req, res) =>{
-	console.log("hola");
 	let index = req.url.indexOf("?")
 	acs_token = req.url.substring(index + 6, req.url.length)
 	console.log('First access_code ->' + acs_token)
